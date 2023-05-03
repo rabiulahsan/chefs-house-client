5644cc22de7295dbb931c0c5af5692c2515383b7
rabiul-ahsan <ra.rabiulahsan@gmail.com>
2023-05-03 15:39:20 +0600

    Project started

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+};
+
+export default App;
