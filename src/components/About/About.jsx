9f5486f04c3a1a81c96cb833e145e9e3e0cd2d0e
rabiul-ahsan <ra.rabiulahsan@gmail.com>
2023-05-04 03:28:21 +0600

    about section added

---
+++ src/components/About/About.jsx
@@ -0,0 +1,34 @@
+/* eslint-disable no-unused-vars */
+import "./About.css";
+import React from "react";
+
+const About = () => {
+  return (
+    <div className=" container-lg my-5 py-5">
+      <h2 className="fw-bolder my-5 text-center">About</h2>
+      <div className=" d-flex justify-content-around py-5 px-3">
+        <div className="about-details my-auto w-50 px-5">
+          <p className="">
+            We all known as a well established restaurent. We have the bset
+            chefs in the country. We also provide chefs for ant program. Our
+            quality is up to mark. All our chefs are so good. There are so many
+            variety of foods you can choose.
+          </p>
+          <p>
+            There is a lots of food item. You can find it in menu or item card.
+            There are also appetaizer and deserts also. We want to provide you a
+            very goood service with good food.
+          </p>
+        </div>
+        <div className="image">
+          <img
+            src="https://media.istockphoto.com/id/1146261080/photo/chef-finishing-healthy-salad-on-a-black-plate-with-tweezers-almost-ready-to-serve-it-on-a.jpg?s=612x612&w=0&k=20&c=78_UgFxxUBLsQcgL2oI6JRDibAqO4hkNldJ_DyA55KU="
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,11 +3,13 @@ import React from "react";
 import "./Home.css";
 
 import Banner from "../Banner/Banner";
+import About from "../About/About";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <About></About>
     </div>
   );
 };
